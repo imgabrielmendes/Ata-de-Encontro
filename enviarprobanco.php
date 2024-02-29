a90fbe335f9326ca49928422bf4cea52dd524094
gabrielm <ggomesmendes2004@gmail.com>
2024-02-29 09:29:27 -0300

    ENVIAR PARA O BANCO DE DADOS FUNCIONANDO (AJAX E ENVIARPROBANCO.PHP)

--- enviarprobanco.php
+++ enviarprobanco.php
@@ -1,16 +1,25 @@
 <?php
 include 'database.php';
 
-	$conteudo=$_POST['texto'];
-	
+session_start();
+$conteudo = $_POST['informacao'];
 
-	$sql = "INSERT INTO assunto (texto)
-	VALUES ('$conteudo')";
-	if (mysqli_query($conn, $sql)) {
-		echo json_encode(array("statusCode"=>200));
-	} 
-	else {
-		echo json_encode(array("statusCode"=>201));
-	}
-	mysqli_close($conn);
+if ($conteudo == "enviar") {
+
+    
+    $enviarbanco = "INSERT INTO assunto (texto) VALUES ('$conteudo')";
+
+    if (mysqli_query($conexao, $enviarbanco)) {
+
+		echo '(3) RECEBEU E ENVIOU PRO BANCO';
+        echo json_encode(array("statusCode" => 200, "message" => "Comando de envio para o banco funcionou"));
+
+    } else {
+
+		echo "(X) A marcação do AJAX não foi identificada";
+        echo json_encode(array("statusCode" => 201, "message" => "Erro ao executar o comando SQL: " . mysqli_error($conexao)));
+    }
+
+    mysqli_close($conexao);
+} 
 ?>
