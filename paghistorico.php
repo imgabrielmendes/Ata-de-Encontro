9fe90b8f5e49ebd57d5615b40e4a90e96fb41d0d
joaothiago <joaothiagormoura@gmail.com>
2024-05-10 11:27:41 -0300

    imprimir

--- paghistorico.php
+++ paghistorico.php
@@ -128,6 +128,7 @@ if ($conn->connect_error) {
         <th class="text-center">Local</th>
         <th class="text-center">Status</th>
         <th class="text-center">Ação</th>
+        <th class="text-center">Imprimir</th>
     </tr>
     </thead>
     <tbody>
@@ -258,10 +259,24 @@ if ($conn->connect_error) {
                     echo "</td>";
                 }     
                 echo "<td>
-                        <button class='btn btn-warning' style='color: white; '>
-                            <a style='color:white; text-decoration:none;' class='text-center align-middle' href='pagatribuida.php? updateid=".$id."'>+</a>
-                        </button>
+                        <a href='pagatribuida.php?updateid=".$id."' class='btn btn-warning' style='color: white;'>
+                            <button class='text-center align-middle' style='color:white; border: none; background: transparent;'>&plus;</button>
+                        </a>
                     </td>";
+
+
+                    echo "<td>
+                    <a class='text-light' href='arquivopdf.php?updateid=".$id."'>
+                    <button class='btn btn-success'>
+                        
+                            <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 384 512' width='16' height='16'>
+                                <path fill='currentColor' d='M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM112 256H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16z'/>
+                            </svg>
+                        </a>
+                    </button>
+                  </td>";
+            
+
                 echo "</tr>";
                     }
                 } else {
@@ -473,5 +488,7 @@ if ($conn->connect_error) {
     <script src="view/js/bootstrap.js"></script>
     <script src="app/gravar.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M128 0C92.7 0 64 28.7 64 64v96h64V64H354.7L384 93.3V160h64V93.3c0-17-6.7-33.3-18.7-45.3L400 18.7C388 6.7 371.7 0 354.7 0H128zM384 352v32 64H128V384 368 352H384zm64 32h32c17.7 0 32-14.3 32-32V256c0-35.3-28.7-64-64-64H64c-35.3 0-64 28.7-64 64v96c0 17.7 14.3 32 32 32H64v64c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V384zM432 248a24 24 0 1 1 0 48 24 24 0 1 1 0-48z"/></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM112 256H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16z"/></svg>
 </body>
 </html>
