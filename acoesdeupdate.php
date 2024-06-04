780932c15fc40e7d10b3725dff03e152c195f736
vitpinheiro <vitoriapinheiro775@gmail.com>
2024-06-04 14:06:38 -0300

    asdasdsa

--- acoesdeupdate.php
+++ acoesdeupdate.php
@@ -3,7 +3,9 @@ include 'database.php';
 // precisa incluir a conexão do outro banco
 session_start();
 
-// texto, facili, delibe é de outro banco de dados, precisa fazer a conexão com o outro também para enviar
+//Falta atualizar deliberadores. Deliberações está atualizando, porém em todos os campos.
+
+$data = $_POST['data'];
 $hora_inicio = $_POST['hora_inicio'];
 $hora_term = $_POST['hora_term'];
 $objetivo = $_POST['objetivo'];
@@ -12,11 +14,11 @@ $tema = $_POST['tema'];
 $texto = $_POST['texto'];
 $id_ataenviar = $_POST['id'];
 
-if (!empty($objetivo) && !empty($local) && !empty($hora_inicio) && !empty($hora_term) && !empty($tema)) {
+if (!empty($objetivo) && !empty($local) && !empty($hora_inicio) && !empty($hora_term) && !empty($tema) ) {
     // Atualização na tabela "assunto" do banco de dados principal
-    $enviarbanco_assunto = "UPDATE assunto SET hora_inicial = ?, hora_termino = ?, objetivo = ?, local = ?, tema = ? WHERE id = ?";
+    $enviarbanco_assunto = "UPDATE assunto SET data_solicitada = ?, hora_inicial = ?, hora_termino = ?, objetivo = ?, local = ?, tema = ? WHERE id = ?";
     if ($stmt_assunto = $conexao->prepare($enviarbanco_assunto)) {
-        $stmt_assunto->bind_param("sssssi", $hora_inicio, $hora_term, $objetivo, $local, $tema, $id_ataenviar);
+        $stmt_assunto->bind_param("ssssssi", $data, $hora_inicio, $hora_term, $objetivo, $local, $tema, $id_ataenviar);
         $stmt_assunto->execute();
         $stmt_assunto->close();
         echo "Dados atualizados com sucesso na tabela 'assunto' do banco de dados principal!";
@@ -24,16 +26,38 @@ if (!empty($objetivo) && !empty($local) && !empty($hora_inicio) && !empty($hora_
         echo "Erro ao preparar a consulta SQL para a tabela 'assunto' do banco de dados principal: " . $conexao->error;
     }
 
-    // Atualização na tabela do outro banco de dados
-    $enviarbanco_outro = "UPDATE textoprinc SET textoprincipal = ? WHERE id_ata = ?";
-    if ($stmt_outro = $conexao_outro->prepare($enviarbanco_outro)) {
-        $stmt_outro->bind_param("si", $texto, $id_ataenviar);
+    // Atualização na tabela "textoprinc"
+    $enviarbanco_outro = "UPDATE textoprinc SET texto_princ = ? WHERE id_ata = ?";
+    if ($stmt_outro = $conexao->prepare($enviarbanco_outro)) {
+        $stmt_outro->bind_param("si", $texto, $id_ataenviar); 
         $stmt_outro->execute();
         $stmt_outro->close();
-        echo "Dados atualizados com sucesso no outro banco de dados!";
+        echo "Dados atualizados com sucesso na tabela 'textoprinc'!";
     } else {
-        echo "Erro ao preparar a consulta SQL para o outro banco de dados: " . $conexao_outro->error;
+        echo "Erro ao preparar a consulta SQL para a tabela 'textoprinc': " . $conexao->error;
     }
-} else {
-    echo "Algum dos campos está vazio.";
+
+  // Atualização na tabela "deliberacoes" para cada ID de deliberação
+foreach ($iddelibe as $deliberacoes) {
+    $enviarbanco_deliberacoes = "UPDATE deliberacoes SET deliberacoes = ? WHERE id_ata = ?";
+    if ($stmt_deliberacoes = $conexao->prepare($enviarbanco_deliberacoes)) {
+        $stmt_deliberacoes->bind_param("si", $deliberacao, $id_ataenviar);
+        $stmt_deliberacoes->execute();
+        $stmt_deliberacoes->close();
+        echo "Dados atualizados com sucesso na tabela 'deliberacoes' para o ID de deliberação $deliberacoes!<br>";
+    } else {
+        echo "Erro ao preparar a consulta SQL para a tabela 'deliberacoes' com o ID de deliberação $deliberacoes: " . $conexao->error . "<br>";
+    }
+}
+
+    
+    $enviarbanco_deliberador = "UPDATE facilitadores SET nome_facilitador = ? WHERE id = ?";
+    if ($stmt_deliberador = $conexao->prepare($enviarbanco_deliberador)) {
+        $stmt_deliberador->bind_param("si", $deliberador, $id_ataenviar); 
+        $stmt_deliberador->execute();
+        $stmt_deliberador->close();
+        echo "Dados atualizados com sucesso na tabela 'facilitadores'!";
+    } else {
+        echo "Erro ao preparar a consulta SQL para a tabela 'facilitadores': " . $conexao->error;
+    }
 }
--- update.php
+++ update.php
@@ -5,8 +5,10 @@ require_once __DIR__ . '/vendor/autoload.php';
 include "conexao.php";
 require_once 'app/acoesform.php';
 $puxarform = new AcoesForm;
+
 $puxaparticipantes = $puxarform->buscarParticipantesPorIdAta($id_ata = "?");
 $id = $_GET['updateid'];
+
 $puxarform = new AcoesForm;
 $pegarfa = $puxarform->pegarfacilitador();
 $pegarlocal = $puxarform->pegarlocais();
@@ -30,10 +32,10 @@ $sql2 = "SELECT
     WHERE ahf.id_ata = $id";
 
 $result2 = mysqli_query($conn, $sql2);
-$facilitadores = array(); 
+$facilitadores = array();
 
-while ($row2 = mysqli_fetch_assoc($result2)) { 
-    $facilitadores[] = $row2;
+while ($row2 = mysqli_fetch_assoc($result2)) {
+  $facilitadores[] = $row2;
 }
 
 $sql3 = "SELECT 
@@ -50,7 +52,7 @@ $deliberacoes_array = array();
 $deliberador_array = array();
 
 if ($result3 && mysqli_num_rows($result3) > 0) {
-    while ($row3 = mysqli_fetch_assoc($result3)) {
+  while ($row3 = mysqli_fetch_assoc($result3)) {
 
     $iddeliberacao_array[] = $row3['id'];
    
@@ -58,9 +60,9 @@ if ($result3 && mysqli_num_rows($result3) > 0) {
 
     $deliberacoesid_array[] = $row3['id_ata'];
 
-        $deliberacoes_array[] = $row3['deliberacoes'];
-        $deliberador_array[] = $row3['deliberador'];
-    }
+    $deliberacoes_array[] = $row3['deliberacoes'];
+    $deliberador_array[] = $row3['deliberador'];
+  }
 }
 
 
@@ -76,8 +78,8 @@ $sql4 = "SELECT
 $result4 = mysqli_query($conn, $sql4);
 $textoprincipal = '';
 
-if ($row4 = mysqli_fetch_assoc($result4)) { 
-    $textoprincipal = $row4['textoprincipal'];
+if ($row4 = mysqli_fetch_assoc($result4)) {
+  $textoprincipal = $row4['textoprincipal'];
 }
 
 mysqli_close($conn);
@@ -87,46 +89,46 @@ mysqli_close($conn);
 <html lang="pt-br">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin - ATA</title>
-    <link rel="icon" href="view/img/Logobordab.png" type="image/x-icon">
-    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="view/css/styles.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Admin - ATA</title>
+  <link rel="icon" href="view/img/Logobordab.png" type="image/x-icon">
+  <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="view/css/styles.css">
   <link rel="stylesheet"
     href="https://cdn.jsdelivr.net/gh/habibmhamadi/multi-select-tag@2.0.1/dist/css/multi-select-tag.css">
-    <script src="view/js/multi-select-tag.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-    <script src="view/js/bootstrap.js"></script>
- 
-    <style>
-        body {
-            background-color: rgba(240, 240, 240, 0.41);
-        }
+  <script src="view/js/multi-select-tag.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+  <script src="view/js/bootstrap.js"></script>
 
-        .content-header {
-            background-color: #001f3f;
-        }
-    </style>
+  <style>
+    body {
+      background-color: rgba(240, 240, 240, 0.41);
+    }
+
+    .content-header {
+      background-color: #001f3f;
+    }
+  </style>
 </head>
 
 <body>
-<header>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-border-hrg">
-        <div class="container-fluid">
+  <header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-border-hrg">
+      <div class="container-fluid">
         <a class="navbar-brand" href="http://10.1.1.31:80/centralservicos/"><img
             src="http://10.1.1.31:80/centralservicos/resources/img/central-servicos.png" alt="Central de Serviço"
             style="width: 160px">
-            </a>
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navBarCentral"
           aria-controls="navBarCentral" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navBarCentral"></div>
-        </div>
-  </nav>
-  <div class="content-header" style="border-bottom: solid 1px gray;">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navBarCentral"></div>
+      </div>
+    </nav>
+    <div class="content-header" style="border-bottom: solid 1px gray;">
       <div class="container-fluid">
         <div class="row py-1">
           <div class="col-sm-6">
@@ -135,38 +137,38 @@ mysqli_close($conn);
         </div>
       </div>
     </div>
-</header>
-<form method="POST" id="" action="">
+  </header>
+  <form method="POST" id="" action="">
     <main class="container_fluid d-flex justify-content-center align-items-center">
       <div class="form-group col-8">
-        <div class="row"> 
+        <div class="row">
           <div class="col-md-12 text-center p-5">
             <h2>INFORMAÇÕES - ATA N°<?php echo $id ?></h2>
           </div>
           <div class="row d-flex justify-content-center align-items-center">
             <div class="col-lg-6 col-xl-4 col-md-6 col-sm-6">
-            <label><b>Data</b></label>
+              <label><b>Data</b></label>
               <input id="datainicio" class="form-control" placeholder="dd-mm-aaaa" min="2024-04-01" type="date"
                 value="<?php echo $datasolicitada; ?>">
-          </div>
+            </div>
             <div class="col-lg-6 col-xl-4 col-md-6 col-sm-6">
-            <label for="nomeMedico"><b>Horário de Início:</b></label>
-            
+              <label for="nomeMedico"><b>Horário de Início:</b></label>
+
               <input class="form-control " type="time" id="horainicio" name="appt" min="" max="18:00"
                 value="<?php echo $horainic ?>">
-          </div>
+            </div>
             <div class="col-lg-6 col-xl-4 col-md-6 col-sm-6">
-            <label for="form-control"> <b> Horário de Término:</b> </label>
+              <label for="form-control"> <b> Horário de Término:</b> </label>
               <input class="form-control " type="time" id="horaterm" name="appt" min="13:00" max="12:00"
                 value="<?php echo $horaterm ?>">
-          </div>
+            </div>
 
 
 
 
             <div class="col-sm-6 col-lg-6 col-md-6 col-xl-2 mt-2">
-            <label for="objetivo pb-2"> <b>Objetivo:</b> </label>
-            <select class="form-control" name="objetivo" id="objetivo">
+              <label for="objetivo pb-2"> <b>Objetivo:</b> </label>
+              <select class="form-control" name="objetivo" id="objetivo">
                 <option value="Reunião" <?php if ($objetivo == 'Reunião')
                   echo 'selected'; ?>>Reunião</option>
                 <option value="Treinamento" <?php if ($objetivo == 'Treinamento')
@@ -174,26 +176,26 @@ mysqli_close($conn);
                 <option value="Consulta" <?php if ($objetivo == 'Consulta')
                   echo 'selected'; ?>>Consulta</option>
                 <?php if (empty($objetivo)): ?>
-                    <option selected disabled hidden>Objetivo não informado</option>
+                  <option selected disabled hidden>Objetivo não informado</option>
                 <?php endif; ?>
-            </select>
-          </div>
-          <div class="col-sm-6 col-md-6 col-xl-5 col-lg-6 mt-2 pb-2">
-          <label for="local"><b>Local:</b></label>
-          <select class="form-control" name="local" id="local">
-              <?php echo empty($pegarlocal) ? '<option selected disabled hidden>Local não informado</option>' : ''; ?>
+              </select>
+            </div>
+            <div class="col-sm-6 col-md-6 col-xl-5 col-lg-6 mt-2 pb-2">
+              <label for="local"><b>Local:</b></label>
+              <select class="form-control" name="local" id="local">
+                <?php echo empty($pegarlocal) ? '<option selected disabled hidden>Local não informado</option>' : ''; ?>
                 <?php foreach ($pegarlocal as $locais): ?>
                   <option value="<?php echo $locais['locais']; ?>" <?php echo ($local == $locais['locais']) ? 'selected' : ''; ?>>
-                      <?php echo $locais['locais']; ?>
+                    <?php echo $locais['locais']; ?>
                   </option>
-              <?php endforeach; ?>
-          </select>
-      </div>
-          <div class="col-md-5 col-xl-5 col-lg-12 col-sm-6 mt-2 pb-2"><b>Tema:</b>
-            
-            <input id="temaprincipal" class="form-control" type="text" value="<?php echo $tema ?>">
-          </div>
-          <div class="row">
+                <?php endforeach; ?>
+              </select>
+            </div>
+            <div class="col-md-5 col-xl-5 col-lg-12 col-sm-6 mt-2 pb-2"><b>Tema:</b>
+
+              <input id="temaprincipal" class="form-control" type="text" value="<?php echo $tema ?>">
+            </div>
+            <div class="row">
               <div class="col-4 mt-2 pt-2 pb-2">
                 <label for="form-control"> <b> Facilitador(es) responsável*:</b> </label>
               </div>
@@ -202,93 +204,94 @@ mysqli_close($conn);
               <optgroup label="Selecione Facilitadores" >
                 <?php foreach ($pegarfa as $facnull): ?>
                   <option  value="<?php echo $facnull['id']; ?>" data-tokens="<?php echo $facnull['nome_facilitador'];  ?>" class="excluir">
-                        <?php echo $facnull['nome_facilitador']; ?>
-                    </option>
+                    <?php echo $facnull['nome_facilitador']; ?>
+                  </option>
                 <?php endforeach ?>
-            </optgroup>
-          </select>
+              </optgroup>
+            </select>
             <div class="col-6  form-control mt-2">
-            <ul>
+              <ul>
                 <?php foreach ($facilitadores as $facilitador): ?>
                     <li><?php echo $facilitador['facilitadores']; ?></li>
                 <?php endforeach; ?>
-            </ul>   
-          </div>
+              </ul>
+
+            </div>
 
 
-    <input type="hidden" name="form_action" value="delete">
-    <input type="hidden" name="delete_id" value="<?php echo $_GET['updateid']; ?>">
-    
+            <input type="hidden" name="form_action" value="delete">
+            <input type="hidden" name="delete_id" value="<?php echo $_GET['updateid']; ?>">
+
             <p class="pt-2 mt-2"><b>Texto Principal:</b></p>
-      <div class="row">
-        <div class="col">
+            <div class="row">
+              <div class="col">
                 <input id="textoprincipal" class="form-control" value="<?php echo $textoprincipal; ?>">
-        </div> 
-      </div>
+              </div>
+            </div>
 
-    <label class="h4 pt-5"><b>DELIBERAÇÕES</b></label>
+            <label class="h4 pt-5"><b>DELIBERAÇÕES</b></label>
             <?php foreach ($deliberacoes_array as $index => $deliberacao): ?>
-    <div class="row pt-3">
-        <div class="col-4">
+              <div class="row pt-3">
+                <div class="col-4">
                   <input id="deliberador" class="form-control" value="<?php echo $deliberador_array[$index] ?>">
-        </div> 
+                </div>
                 <input id="deliberacao" class="col form-control" value="<?php echo $deliberacao ?>">
                 <?php
 
                 ?>
-    </div>
-    <?php endforeach; ?>
-    
-</form>
+              </div>
+            <?php endforeach; ?>
 
-<form id="seu-formulario-id">
+  </form>
+
+  <form id="seu-formulario-id">
     <input type="hidden" name="id" value="<?php echo $_GET['updateid']; ?>">
     <div class="row">
-        <div class="col-3 mt-3">
-            <div class="btn-atas">
-                <button type="submit" class="btn btn-primary">Atualizar</button>
-            </div>
-        </div>
-    </div>
-</form>
-
-          <div class="row">
-            <footer class="col main-footer p-4" style="margin-left: 0 !important; margin-top: 1em;">
-      <strong>Copyright © 2021 <a href="http://www.hospitalriogrande.com.br/" target="_blank">Hospital Rio
-          Grande</a></strong>. Todos os direitos reservados.
-              <div class="float-right d-none d-sm-inline-block">
-                <b>Versão</b> 0.0.1
-            </footer>
-          </div>
+      <div class="col-3 mt-3">
+        <div class="btn-atas">
+          <button type="submit" class="btn btn-primary">Atualizar</button>
         </div>
       </div>
-    </main>
+    </div>
+  </form>
 
-    <script>
+  <div class="row">
+    <footer class="col main-footer p-4" style="margin-left: 0 !important; margin-top: 1em;">
+      <strong>Copyright © 2021 <a href="http://www.hospitalriogrande.com.br/" target="_blank">Hospital Rio
+          Grande</a></strong>. Todos os direitos reservados.
+      <div class="float-right d-none d-sm-inline-block">
+        <b>Versão</b> 0.0.1
+    </footer>
+  </div>
+  </div>
+  </div>
+  </main>
 
-      new MultiSelectTag('selecionandofacilitador', {
-            rounded: true, 
-            shadow: false,     
-            placeholder: 'Search', 
-            tagColor: {
-                textColor: '#1C1C1C',
-                borderColor: '#4F4F4F',
-                bgColor: '#F0F0F0',
-            },
+  <script>
+
+    new MultiSelectTag('selecionandofacilitador', {
+      rounded: true,
+      shadow: false,
+      placeholder: 'Search',
+      tagColor: {
+        textColor: '#1C1C1C',
+        borderColor: '#4F4F4F',
+        bgColor: '#F0F0F0',
+      },
             onChange: function(selected_ids, selected_names) {
-                facilitadoresSelecionados = selected_ids;
-                facilitadoresSelecionadosLabel = selected_names;
+        facilitadoresSelecionados = selected_ids;
+        facilitadoresSelecionadosLabel = selected_names;
                 // console.log(facilitadoresSelecionados);
                 // console.log(facilitadoresSelecionadosLabel);
-            }
-      });
+      }
+    });
 
     document.addEventListener('DOMContentLoaded', function () {
-    var form = document.getElementById("seu-formulario-id");
+      var form = document.getElementById("seu-formulario-id");
 
       form.addEventListener('submit', function (event) {
         event.preventDefault();
-       
+
         var data = document.getElementById('datainicio').value;
         var dataf = formatarData(data);
         var hora_inicio = document.getElementById('horainicio').value;
@@ -298,40 +301,40 @@ mysqli_close($conn);
         var tema = document.getElementById('temaprincipal').value;
 
         var texto = document.getElementById('textoprincipal').value;
-        var id = <?php echo json_encode($_GET['updateid']); ?>;
+        var id = <?php echo ($_GET['updateid']); ?>;
         console.log(texto);
       // texto é de outro banco de dados
 
        
         if (objetivo === "" || local === "" || hora_inicio === "" || hora_term === "" || tema === "" || texto === "" ) {
-            window.alert("Preencha as informações");
+          window.alert("Preencha as informações");
         } else {
-            $.ajax({
-                type: 'POST',
-                url: 'acoesdeupdate.php',
-                data: {
-                    objetivo: objetivo,
-                    id: id,
-                    local: local,
+          $.ajax({
+            type: 'POST',
+            url: 'acoesdeupdate.php',
+            data: {
+              objetivo: objetivo,
+              id: id,
+              local: local,
               hora_inicio: hora_inicio,
-                    hora_term: hora_term,
-                    tema: tema,
-                    texto: texto,
-data: data,
+              hora_term: hora_term,
+              tema: tema,
+              texto: texto,
+              data: data,
         
-                },
+            },
             success: function (response) {
-                    alert(response);
-                    console.log("DEU CERTO");
-                },
+              alert(response);
+              console.log("DEU CERTO");
+            },
             error: function (xhr, status, error) {
-                    console.error(xhr.responseText);
-                    console.log("DEU RUIM");
-                }
-            });
+              console.error(xhr.responseText);
+              console.log("DEU RUIM");
+            }
+          });
         }
+      });
     });
-});
 
 
 // function formatarData(data) {
@@ -350,9 +353,9 @@ data: data,
 
       // Formata a data como "dia/mês/ano" (por exemplo, "28/05/2024")
       return dia + '/' + mes + '/' + ano;
-    }
+    
 
-</script>
+  </script>
 </body>
 
 </html>
