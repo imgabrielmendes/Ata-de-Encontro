623e3c2b53339fd10a4b0e69d64dd06c93afe700
PedroHenriqueCustodiodelima <ph4261009@gmail.com>
2024-06-06 11:20:15 -0300

    ajustes no modal participante

--- pagatribuida.php
+++ pagatribuida.php
@@ -335,7 +335,14 @@ document.getElementById('registrarparticipantes').addEventListener('click', func
   <div class="modal-dialog modal-dialog-centered modal-xl modal-fullscreen">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="listaParticipantesModalLabel">Participantes do encontro</h5>
+      <h4 class="modal-title" id="listaParticipantesModalLabel">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" style="width: 1em; height: 1em; margin-right: 0.5em;">
+        <path d="M72 88a56 56 0 1 1 112 0A56 56 0 1 1 72 88zM64 245.7C54 256.9 48 271.8 48 288s6 31.1 16 42.3V245.7zm144.4-49.3C178.7 222.7 160 261.2 160 304c0 34.3 12 65.8 32 90.5V416c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V389.2C26.2 371.2 0 332.7 0 288c0-61.9 50.1-112 112-112h32c24 0 46.2 7.5 64.4 20.3zM448 416V394.5c20-24.7 32-56.2 32-90.5c0-42.8-18.7-81.3-48.4-107.7C449.8 183.5 472 176 496 176h32c61.9 0 112 50.1 112 112c0 44.7-26.2 83.2-64 101.2V416c0 17.7-14.3 32-32 32H480c-17.7 0-32-14.3-32-32zm8-328a56 56 0 1 1 112 0A56 56 0 1 1 456 88zM576 245.7v84.7c10-11.3 16-26.1 16-42.3s-6-31.1-16-42.3zM320 32a64 64 0 1 1 0 128 64 64 0 1 1 0-128zM240 304c0 16.2 6 31 16 42.3V261.7c-10 11.3-16 26.1-16 42.3zm144-42.3v84.7c10-11.3 16-26.1 16-42.3s-6-31.1-16-42.3zM448 304c0 44.7-26.2 83.2-64 101.2V448c0 17.7-14.3 32-32 32H288c-17.7 0-32-14.3-32-32V405.2c-37.8-18-64-56.5-64-101.2c0-61.9 50.1-112 112-112h32c61.9 0 112 50.1 112 112z"/>
+    </svg>
+    Participantes do encontro
+</h4>
+
+
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
@@ -535,7 +542,7 @@ function adicionarParticipanteAoLabel(participante) {
                             }
                             foreach ($deliberacoesAgrupadas as $conteudo => $deliberadores) {
                                 $deliberadoresStr = implode(', ', $deliberadores);
-                                ?>
+                        ?>
                                 <div style="margin-bottom: 15px;" data-conteudo="<?php echo $conteudo; ?>">
                                     <li class="form-control bg-body-secondary border rounded">
                                         <div>
@@ -546,13 +553,14 @@ function adicionarParticipanteAoLabel(participante) {
                                         <div>
                                             <strong>Deliberação:</strong> <?php echo $conteudo; ?>
                                         </div>
-                                        <button class="btn btn-danger btn-sm delete-deliberacao" data-id-ata="<?php echo $id_ata; ?>" data-conteudo="<?php echo $conteudo; ?>">Excluir</button>
                                     </li>
+                                    <button class="btn btn-danger btn-sm delete-deliberacao float-right mt-2" data-id-ata="<?php echo $id_ata; ?>" data-conteudo="<?php echo $conteudo; ?>">Excluir</button>
                                 </div>
-                                <?php
+                        <?php
                             }
                         }
                         ?>
+
                     </ul>
                 </div>          
             </div>
@@ -631,20 +639,6 @@ function adicionarParticipanteAoLabel(participante) {
     <div>
     <div class="row">
         <div class="col text-center">
-            <style>
-                #alertIcon {
-                    width: 24px;
-                    height: auto;
-                    margin-right: 5px;
-                    cursor: pointer;
-                }
-                .btn-container {
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    gap: 10px; 
-                }
-            </style>
             <div class="btn-container">
             <button id="reloadPageButton" type="button" class="btn btn-secondary">Inserir deliberação</button>
                 <button id="atribuida" class="btn btn-primary">Finalizar encontro</button>
