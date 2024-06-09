0c1d54f796d57ca1b7bf6aa2f0b0b2c216ad74d4
Pedro Henrique Custodio de Lima <lima.custodio@escolar.ifrn.edu.br>
2024-06-08 21:02:03 -0300

    ajustes nas deliberações existentes

--- pagatribuida.php
+++ pagatribuida.php
@@ -540,28 +540,155 @@ if (!empty($deliberacoes)) {
 
         $deliberacoesAgrupadas[$conteudo][] = $deliberador;
     }
+    echo "<table class='table'>";
+    echo "<thead><tr><th>Deliberador</th><th>Deliberação</th><th>Editar</th><th>Excluir</th></tr></thead>";
+    echo "<tbody>";
     foreach ($deliberacoesAgrupadas as $conteudo => $deliberadores) {
         $deliberadoresStr = implode(', ', $deliberadores);
+        // Cria um ID único para cada modal de edição
+        $modal_editar_id = 'editarModal_' . uniqid();
 ?>
-        <div style="margin-bottom: 15px;" data-conteudo="<?php echo $conteudo; ?>">
-            <div class="form-control bg-body-secondary border rounded smaller-width">
-                <strong>Deliberador:</strong> <?php echo $deliberadoresStr; ?>
-            </div>
-            <div class="form-control border rounded smaller-width">
-                <strong>Deliberação:</strong> <?php echo $conteudo; ?>
-            </div>
-            <div class="float-right"> <!-- Adicionando a classe float-right -->
-                <button class="btn btn-danger btn-sm delete-deliberacao mt-2" data-id-ata="<?php echo $id_ata; ?>" data-conteudo="<?php echo $conteudo; ?>">
+        <tr class="table-row" data-conteudo="<?php echo $conteudo; ?>">
+            <td><?php echo $deliberadoresStr; ?></td>
+            <td><?php echo strlen($conteudo) > 50 ? substr($conteudo, 0, 50) . '...' : $conteudo; ?></td>
+            <td>
+                <!-- Botão de editar -->
+                <button class='btn btn-warning btn-sm edit-deliberacao' data-bs-toggle="modal" data-bs-target="#<?php echo $modal_editar_id; ?>">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="width: 1em; height: 1em;">
+                        <path fill="#ffffff" d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/>
+                    </svg>
+                </button>
+
+                <!-- Modal de edição -->
+                <div class="modal fade" id="<?php echo $modal_editar_id; ?>" tabindex="-1" aria-labelledby="editarModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="editarModalLabel">Editar Deliberação</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+    <form>
+        <div class="mb-3">
+            <label for="editInput" class="form-label">Editar Deliberação</label>
+            <input type="text" class="form-control" id="editInput" placeholder="Digite a nova deliberação">
+        </div>
+        <button type="submit" class="btn btn-primary">Salvar</button>
+    </form>
+</div>
+
+                        </div>
+                    </div>
+                </div>
+            </td>
+            <td>
+                <button class="btn btn-danger btn-sm delete-deliberacao" data-id-ata="<?php echo $id_ata; ?>" data-conteudo="<?php echo $conteudo; ?>">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="mr-2" style="width: 1em; height: 1em;">
                         <path fill="#ffffff" d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/>
                     </svg>
                 </button>
-            </div>
-        </div>
+            </td>
+        </tr>
 <?php
     }
+    echo "</tbody></table>";
 }
 ?>
+
+<!-- Modal -->
+<div class="modal fade" id="infoModal" tabindex="-1" aria-labelledby="infoModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="infoModalLabel">Detalhes da Deliberação</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <h5>Deliberações</h5>
+                <div class="card">
+                    <div class="card-body">
+                        <p id="deliberationContent" style="height: 200px; overflow-y: auto;"></p>
+                    </div>
+                </div>
+                <div class="mt-3">
+                    <strong>Deliberador:</strong>
+                    <p id="deliberator"></p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+<script>
+document.addEventListener("DOMContentLoaded", function() {
+    const tableRows = document.querySelectorAll(".table-row");
+
+    tableRows.forEach(row => {
+        row.addEventListener("click", function(event) {
+            if (event.target.closest("button")) {
+                return; // Não faça nada se um botão foi clicado
+            }
+
+            const conteudo = this.getAttribute("data-conteudo");
+            const deliberadores = this.querySelector("td:first-child").innerText;
+
+            document.getElementById('deliberationContent').innerText = conteudo;
+            document.getElementById('deliberator').innerText = deliberadores;
+
+            const infoModal = new bootstrap.Modal(document.getElementById('infoModal'));
+            infoModal.show();
+        });
+    });
+
+    const editButtons = document.querySelectorAll(".edit-deliberacao");
+
+    editButtons.forEach(button => {
+        button.addEventListener("click", function(event) {
+            const modalId = this.getAttribute("data-bs-target");
+            const modal = new bootstrap.Modal(document.querySelector(modalId));
+            
+            // Verifica se o modal da linha da tabela está aberto e fecha-o
+            const infoModal = document.getElementById('infoModal');
+            const isInfoModalOpen = infoModal.classList.contains('show');
+            if (isInfoModalOpen) {
+                const infoModalInstance = bootstrap.Modal.getInstance(infoModal);
+                infoModalInstance.hide();
+            }
+            
+            // Abre o modal de edição
+            modal.show();
+        });
+    });
+
+    const deleteButtons = document.querySelectorAll(".delete-deliberacao");
+
+    deleteButtons.forEach(button => {
+        button.addEventListener("click", function(event) {
+            event.stopPropagation();
+            event.preventDefault();
+            const idAta = this.getAttribute('data-id-ata');
+            const conteudo = this.getAttribute('data-conteudo');
+
+            console.log("Excluir deliberação:", conteudo, "da ata:", idAta);
+            location.reload();
+        });
+    });
+});
+
+</script>
+
+
+
+
+
+
+
 
 
 
