297d9844ba2d8a46ac81f992e5657d93b7ce1b4f
PedroHenriqueCustodiodeLima <ph4261009@gmail.com>
2024-06-19 10:59:09 -0300

    blibioteca de email

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -7,6 +7,10 @@ namespace Composer\Autoload;
 class ComposerStaticInit3ba5865ee439cf319452b0df1a1dfb7c
 {
     public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'PHPMailer\\PHPMailer\\' => 20,
+        ),
         'A' => 
         array (
             'App\\' => 4,
@@ -14,6 +18,10 @@ class ComposerStaticInit3ba5865ee439cf319452b0df1a1dfb7c
     );
 
     public static $prefixDirsPsr4 = array (
+        'PHPMailer\\PHPMailer\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phpmailer/phpmailer/src',
+        ),
         'App\\' => 
         array (
             0 => __DIR__ . '/../..' . '/app',
